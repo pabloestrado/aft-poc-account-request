0a4847240c5202411b7328a4352a491eb1bac584
Pavlo Romaniuk <pavlo.romaniuk@automat-it.com>
2024-10-03 16:54:05 +0300

    Add new account

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,3 +28,36 @@ module "sandbox3" {
 
   account_customizations_name = "sandbox"
 }
+
+
+
+module "sandbox4" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "awsacct+spare28+sandbox-aft-test-request-4@automat-it.com"
+    AccountName               = "sandbox-aft-test-request-4"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "awsacct+spare28+sandbox-aft-test-request-4@automat-it.com"
+    SSOUserFirstName          = "Sandbox"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group        = "non-prod"
+    env          = "dev"
+    provect_name = "aft-poc"
+    vpc_cidr     = "10.130.0.0/16"
+  }
+
+  account_customizations_name = "eks-public"
+}
