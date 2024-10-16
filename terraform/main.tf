d0c7a2e5480c4487a90d1f2e579e41233c668598
Pavlo Romaniuk <pavlo.romaniuk@automat-it.com>
2024-10-16 12:07:07 +0300

    cleanup

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,37 +1,4 @@
-module "sandbox3" {
-  source = "./modules/aft-account-request"
-
-  control_tower_parameters = {
-    AccountEmail              = "awsacct+spare28+sandbox-aft-test-request-2@automat-it.com"
-    AccountName               = "sandbox-aft-test-request-2"
-    ManagedOrganizationalUnit = "Sandbox"
-    SSOUserEmail              = "awsacct+spare28+sandbox-aft-test-request-2@automat-it.com"
-    SSOUserFirstName          = "Sandbox"
-    SSOUserLastName           = "AFT"
-  }
-
-  account_tags = {
-    "Learn Tutorial" = "AFT"
-  }
-
-  change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
-  }
-
-  custom_fields = {
-    group        = "non-prod"
-    env          = "dev"
-    project_name = "aft-poc"
-    vpc_cidr     = "10.128.0.0/16"
-  }
-
-  account_customizations_name = "sandbox"
-}
-
-
-
-module "sandbox4" {
+module "eks-public-module" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
@@ -53,7 +20,6 @@ module "sandbox4" {
   }
 
   custom_fields = {
-    group        = "non-prod"
     env          = "dev"
     project_name = "aft-poc"
     vpc_cidr     = "10.130.0.0/16"
@@ -85,7 +51,6 @@ module "mgmt-public-module" {
   }
 
   custom_fields = {
-    group        = "non-prod"
     env          = "mgmt"
     project_name = "aft-poc"
     vpc_cidr     = "10.131.0.0/16"
@@ -117,7 +82,6 @@ module "mgmt" {
   }
 
   custom_fields = {
-    group        = "non-prod"
     env          = "mgmt"
     project_name = "aft-poc"
     vpc_cidr     = "10.132.0.0/16"
